c563422d7f0ddf465edbe39c90673bf31d5e6740
LagrangeH <maxsergeev39@gmail.com>
2020-11-24 22:35:55 +0300

    0.1.1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Documentation: https://python-ev3dev.readthedocs.io/en/latest/
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
@@ -9,7 +10,7 @@ from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from time import sleep
 
 
-drive = MoveTank(OUTPUT_A, OUTPUT_B)
+wheels = MoveTank(OUTPUT_A, OUTPUT_B)
 motor = LargeMotor(OUTPUT_C)
 color_left, color_right, color_center = ColorSensor(INPUT_2), ColorSensor(INPUT_3), ColorSensor(INPUT_4)
 infrared = InfraredSensor(INPUT_1)
